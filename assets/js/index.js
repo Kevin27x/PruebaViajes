53677d321ffc583f1559667e3f996d6396271e03
KevinC <Kevin.collao1@gmail.com>
2022-01-17 18:57:54 -0300

    ultima actualizacion

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,57 +3,67 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 });
+
+/* Activacion modal para cards*/
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
 /* JQUERY */
 $(document).ready(function(){
-  //Cambio de color h2 :hover
+//Cambio de color h2 :hover
   $("h2 ").hover(function(){
-    //cursor dentro
+//cursor dentro
     $("h2 ").css("color","#17a3b8");
   },
   function(){
-    //cursor fuera
+//cursor fuera
     $("h2 ").css("color","inherit");
   });
 
-   //Cambio de color iconos :hover
+//Cambio de color iconosfb :hover
    $(".fa-facebook-square").hover(function(){
-    //cursor dentro
+    
     $(".fa-facebook-square").css("color","#2B303A");
   },
   function(){
-    //cursor fuera
+    
     $(".fa-facebook-square").css("color","inherit");
   });
 
 
-//
+//Cambio de color iconotwitter :hover
 
   $(".fa-twitter-square").hover(function(){
-    //cursor dentro
+    
     $(".fa-twitter-square").css("color","#2B303A");
   },
   function(){
-    //cursor fuera
+    
     $(".fa-twitter-square").css("color","inherit");
   });
 
-//
-$(".fa-linkedin").hover(function(){
-  //cursor dentro
-  $(".fa-linkedin").css("color","#2B303A");
-},
-function(){
-  //cursor fuera
-  $(".fa-linkedin").css("color","inherit");
-});
-//
-$(".fa-github-square").hover(function(){
-  //cursor dentro
-  $(".fa-github-square").css("color","#2B303A");
-},
-function(){
-  //cursor fuera
-  $(".fa-github-square").css("color","inherit");
-});
+//Cambio de color iconolinkedin :hover
+  $(".fa-linkedin").hover(function(){
+    
+    $(".fa-linkedin").css("color","#2B303A");
+  },
+  function(){
+    
+    $(".fa-linkedin").css("color","inherit");
+  });
+//Cambio de color iconogithub :hover
+  $(".fa-github-square").hover(function(){
+    
+    $(".fa-github-square").css("color","#2B303A");
+  },
+  function(){
+    
+    $(".fa-github-square").css("color","inherit");
+  });
+
+
 
 });
